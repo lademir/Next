57941c150bcdeacb4aef138d61d9942d520c71a9
Saulo Ferreira de Souza <saulosouza.dev@gmail.com>
2021-10-13 17:04:12 -0300

    integracao com API #02

---
+++ next-basico/src/pages/api/form.js
@@ -0,0 +1,26 @@
+const usuarios = []
+
+
+export default function fomr(req, res) {
+
+    if(req.method === "POST"){
+        post(req, res)
+
+    } else if (req.method === "GET"){
+        get(req, res)
+
+    }
+    else {
+        res.status(405).send()
+    }
+
+}
+
+function post(req, res) {
+    const usuario = JSON.parse(req.body)
+    usuarios.push(usuario)
+    res.status(200).send()
+}
+function get(req, res) {
+    res.status(200).json(usuarios)
+}
