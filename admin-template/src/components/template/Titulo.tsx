a8aae727b1b91ae5f85e47218e66203331835e21
lademir <lademirjunior@hotmail.com>
2021-10-22 08:55:37 -0300

    finalizando cores do darkMode e atualizando a fonte

--- admin-template/src/components/template/Titulo.tsx
+++ admin-template/src/components/template/Titulo.tsx
@@ -9,10 +9,11 @@ export default function Layout(props:TituloProps) {
     return (
         <div>
             <h1 className={`
-                font-black
+                font-black text-3xl
+                text-gray-900 dark:text-gray-100
             `}>{props.titulo}</h1>
             <h2 className={`
-
+                font-light text-sm text-gray-600 dark:text-gray-200
             `}>{props.subtitulo}</h2>
         </div>
     )
