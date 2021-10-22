f55edc9a268d5b767af5dbc4db88fe392fda77de
lademir <lademirjunior@hotmail.com>
2021-10-22 12:04:29 -0300

    finalizando front da tela de login

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -1,15 +1,11 @@
 import Layout from "../components/template/Layout"
-import useAppData from "../data/hook/useAppData"
 
 export default function Notificacoes() {
-
-  const { alternarTema} = useAppData()
 
   return (
     <Layout titulo="Notificações"
      subtitulo="Aqui voce ira gerencias as suas notificacoes">
-       
-       <button onClick={alternarTema}>onClick</button>
+      <h1>Notificações</h1>
     </Layout>
   )
 }
